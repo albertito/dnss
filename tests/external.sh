cdded2209e09a57e53e63736664a980495a012c1
Alberto Bertogli <albertito@blitiri.com.ar>
2020-05-14 00:48:58 +0100

    tests: Remove securedns.eu which has been shutdown
    This patch removes securedns.eu from our external test list, since it
    shut down on 2020-04-30.

--- tests/external.sh
+++ tests/external.sh
@@ -191,7 +191,6 @@ for server in \
 	"https://dns.quad9.net/dns-query" \
 	"https://doh.cleanbrowsing.org/doh/family-filter/" \
 	"https://doh.powerdns.org" \
-	"https://doh.securedns.eu/dns-query" \
 	;
 do
 	echo "## DoH against $server"
