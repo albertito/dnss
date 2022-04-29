61ea47a6094d4b06d49e925507fd7e356925929c
Alberto Bertogli <albertito@blitiri.com.ar>
2022-04-29 11:40:07 +0100

    modules: Update dependencies' versions
    This patch updates the versions of our dependencies, just to keep up
    with general bug fixes. No API changes or compatibility issues are
    expected.
    
    We also remove an unnecessary dependency on golang.org/x/net/idna while
    we're at it.

--- go.mod
+++ go.mod
@@ -1,11 +1,13 @@
 module blitiri.com.ar/go/dnss
 
-go 1.14
+go 1.15
 
 require (
 	blitiri.com.ar/go/log v1.1.0
 	blitiri.com.ar/go/systemd v1.1.0
-	github.com/miekg/dns v1.1.42
-	golang.org/x/net v0.0.0-20210525063256-abc453219eb5
-	golang.org/x/sys v0.0.0-20210603125802-9665404d3644 // indirect
+	github.com/miekg/dns v1.1.48
+	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4
+	golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
+	golang.org/x/tools v0.1.10 // indirect
+	golang.org/x/xerrors v0.0.0-20220411194840-2f41105eb62f // indirect
 )
--- tests/generate_cert.go
+++ tests/generate_cert.go
@@ -23,8 +23,6 @@ import (
 	"os"
 	"strings"
 	"time"
-
-	"golang.org/x/net/idna"
 )
 
 var (
@@ -83,13 +81,7 @@ func main() {
 		if ip := net.ParseIP(h); ip != nil {
 			template.IPAddresses = append(template.IPAddresses, ip)
 		} else {
-			// We use IDNA-encoded DNS names, otherwise the TLS library won't
-			// load the certificates.
-			ih, err := idna.ToASCII(h)
-			if err != nil {
-				log.Fatalf("host %q cannot be IDNA-encoded: %v", h, err)
-			}
-			template.DNSNames = append(template.DNSNames, ih)
+			template.DNSNames = append(template.DNSNames, h)
 		}
 	}
 
